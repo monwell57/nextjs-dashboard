125755a9ac3b2edb8ddc69b4e7a4686004e26f6a
Gregory Peters <greg.monwell57@gmail.com>
2023-12-21 09:30:11 -0800

    Added Alt to Images

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,7 +27,7 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
-                        alt=""
+                        alt="table"
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
